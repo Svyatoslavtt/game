89cfcc65c1684645785be4fc625bf3d7325cdc30
svyatoslav <svyatoslav.t@2muchcoffee.com>
2020-07-02 10:55:43 +0300

    Updates

---
+++ precache-manifest.e4af5e6928e3abef83d1504765272871.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8611568d3e044dbe200a944fbb52f862",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "6a83e54a01cbf20a87ee",
+    "url": "/game/static/css/main.88edfeda.chunk.css"
+  },
+  {
+    "revision": "c6038fff19af28a4efd3",
+    "url": "/game/static/js/2.a460f652.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game/static/js/2.a460f652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a83e54a01cbf20a87ee",
+    "url": "/game/static/js/main.ea109fde.chunk.js"
+  },
+  {
+    "revision": "be523661599ed65828e4",
+    "url": "/game/static/js/runtime-main.eb6a8fc6.js"
+  },
+  {
+    "revision": "fca6f398acfaedfea3a43a624df52a36",
+    "url": "/game/static/media/derevo_planeta_zvezdy_galaktika_art_117068_1280x1024.fca6f398.jpg"
+  },
+  {
+    "revision": "b3bef5486c2eba7da624d12dc7e57366",
+    "url": "/game/static/media/gory_derevya_dal_sneg_oblaka_115484_1920x1080.b3bef548.jpg"
+  },
+  {
+    "revision": "0b51f8bd07dbf86adcb8eb68544326e8",
+    "url": "/game/static/media/gory_sneg_vershina_84607_1920x1080.0b51f8bd.jpg"
+  },
+  {
+    "revision": "ca1da7ca94820683623a6ad800fc590e",
+    "url": "/game/static/media/image.ca1da7ca.jpg"
+  },
+  {
+    "revision": "3240acb250cbbd000627ef1067634c27",
+    "url": "/game/static/media/mountain_range_himalayas_mountains_sky_fog_103464_1920x1080.3240acb2.jpg"
+  },
+  {
+    "revision": "5f3f085f2bedda973b5cc8b09441122f",
+    "url": "/game/static/media/pejzazh_gory_ozero_160353_1280x1024.5f3f085f.jpg"
+  }
+]);
